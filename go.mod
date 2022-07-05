e40e1d9de8eb394b6d756e6522ac3bd0040e3a14
Stefano Oliveira <oliveira.stefano@gmail.com>
2022-07-05 11:23:34 -0300

    Entidades - Regras de Negocio

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/oliveira-stefano/ddd-patterns-go
+
+go 1.18
--- src/entity/customer.go
+++ src/entity/customer.go
@@ -14,3 +14,15 @@ func (c *Customer) NewCustomer(id string, name string, address string, active bo
 	c.address = address
 	c.active = active
 }
+
+func (c *Customer) ChangeName(name string) {
+	c.name = name
+}
+
+func (c *Customer) Activate() {
+	c.active = true
+}
+
+func (c *Customer) Deactivate() {
+	c.active = false
+}
