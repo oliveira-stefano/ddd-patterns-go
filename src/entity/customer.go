505faa9872b7e2bf677405be9ccccecd638d7de7
Stefano Oliveira <oliveira.stefano@gmail.com>
2022-07-05 11:43:59 -0300

    Entidades - principio da autovalidacao

--- src/entity/customer.go
+++ src/entity/customer.go
@@ -3,26 +3,37 @@ package entity
 type Customer struct {
 	id      string
 	name    string
-	address string
-	active  bool
+	address string `default:""`
+	active  bool   `default:"false"`
 }
 
 // NewCustomer equivalente ao construtor de Customer
-func (c *Customer) NewCustomer(id string, name string, address string, active bool) {
+func (c *Customer) NewCustomer(id string, name string) {
 	c.id = id
 	c.name = name
-	c.address = address
-	c.active = active
+	c.Validate()
 }
 
 func (c *Customer) ChangeName(name string) {
 	c.name = name
+	c.Validate()
 }
 
 func (c *Customer) Activate() {
+	if c.address == "" {
+		panic("Address is mandatory to activate a customer")
+	}
 	c.active = true
 }
 
 func (c *Customer) Deactivate() {
 	c.active = false
+}
+
+func (c *Customer) Validate() {
+	if c.name == "" {
+		panic("Customer name is required")
+	} else if c.id == "" {
+		panic("Customer id is required")
+	}
 }
